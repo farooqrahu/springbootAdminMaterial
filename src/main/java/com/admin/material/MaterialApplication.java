f917412119eae9de598477ba62b0b1d9c8c4545d
Farooq Rahu <farooqrahu@hotmail.com>
2019-03-28 11:40:15 +0500

    first commit

---
+++ src/main/java/com/admin/material/MaterialApplication.java
@@ -0,0 +1,13 @@
+package com.admin.material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/admin/material/SimpleController.java
@@ -0,0 +1,38 @@
+package com.admin.material;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@Controller
+public class SimpleController {
+    private static final Logger logger = Logger.getLogger(SimpleController.class);
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        return "index";
+    }
+
+//    pages/forms/basic-form-elements.html
+    @RequestMapping(value = "/pages/forms/{page}", method = RequestMethod.GET)
+    public String pages(Locale locale, Model model, @PathVariable(value="page") String page) {
+        logger.info("Page Called: "+page);
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        model.addAttribute("serverTime", formattedDate );
+
+        return "/pages/forms/"+page;
+    }
+
+}
